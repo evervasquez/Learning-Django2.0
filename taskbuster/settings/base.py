1ee6b36cf5c80f5411bbcfbfa76ac17377258410
eveR Vásquez <pever@unsm.edu.pe>
2017-06-03 23:33:35 -0500

    add test home

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'taskbuster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -94,3 +94,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configuration
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
